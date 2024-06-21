8f4c53323ced008dfc950c108812fab18e829024
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-06-21 16:59:06 +0530

    movie mania part 2 done

--- movie-mania/src/index.js
+++ movie-mania/src/index.js
@@ -1,7 +1,11 @@
 import { getMovieReviewData } from "./data.js";
 
+let sortDesc = false;
+
 function init() {
   const movieReviewData = getMovieReviewData();
+
+  registerHandlers(movieReviewData);
   paintStatistics(movieReviewData);
   paintMovieData(movieReviewData);
 }
@@ -36,12 +40,13 @@ function addStat(elem, value) {
 
 function paintMovieData(movieReviewData) {
   const flatReviewData = movieReviewData.flat();
+  const sorted = flatReviewData.toSorted((a, b) => b.on - a.on);
   const movieListEL = document.querySelector("#movieListId UL");
+  addMovieReviewData(movieListEL, sorted);
+}
 
-  console.log(movieListEL);
-
-  flatReviewData.map((movie) => {
-    console.log(movie);
+function addMovieReviewData(movieListEL, movieReview) {
+  movieReview.map((movie) => {
 
     const liElem = document.createElement("li");
     liElem.classList.add("card", "p-2", "my-2");
@@ -63,6 +68,68 @@ function paintMovieData(movieReviewData) {
 
     movieListEL.appendChild(liElem);
   })
+}
+
+function registerHandlers(movieReviewData) {
+  const sortBtn = document.getElementById("srtBtnId");
+  const grpBtn = document.getElementById("grpBtnId");
+  
+  sortBtn.addEventListener("click", () => sortByReview(movieReviewData));
+  grpBtn.addEventListener("click",  () => groupReviewsByTitle(movieReviewData)); 
+}
+
+function sortByReview(movieReviewData) {
+    sortDesc = !sortDesc;
+    const flatReviewData = movieReviewData.flat();
+    let sortedReviewData = sortDesc
+        ? flatReviewData.toSorted((a, b) => b.rating - a.rating)
+        : flatReviewData.toSorted((a, b) => a.rating - b.rating);
+    const movieListEL = document.querySelector("#movieListId UL");
+    removeAllChildNodes(movieListEL);
+    addMovieReviewData(movieListEL, sortedReviewData);
+}
+
+function groupReviewsByTitle(movieReviewData) {
+  console.log("groupReviewsByTitle", movieReviewData);
+  const flatReviewData = movieReviewData.flat();
+  const groupdReviews = Object.groupBy(flatReviewData, ({title}) => title);
+  console.log(groupdReviews);
+
+  const titleKeys = Reflect.ownKeys(groupdReviews);
+  console.log(titleKeys);
+
+  const movieListEL = document.querySelector("#movieListId UL");
+  removeAllChildNodes(movieListEL);
+
+  titleKeys.forEach((title) => {
+    const liEl = document.createElement("li");
+    liEl.classList.add("card", "my-2",);
+
+    const hEl = document.createElement("h2");
+    hEl.classList.add("text-3xl");
+    hEl.innerText = title;
+    liEl.appendChild(hEl);
+
+    const reviews = groupdReviews[title];
+    console.log(reviews);
+
+    reviews.forEach((review) => {
+      const pEl = document.createElement("p");
+      pEl.classList.add("mx-2", "my-2");
+      const message = `✅ <strong>${review.by}</strong> has given <strong>${review.rating}</strong> rating with a comment, <i>${review.content}</i>`;
+      pEl.innerHTML = message;
+      liEl.appendChild(pEl);
+    })
+
+    movieListEL.appendChild(liEl);
+  })
+
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
 }
 
 init();
